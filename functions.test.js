27a75af1b80d1d6b6c2f8bee5d8ec14414d8ab4b
abhidatta0 <abhidatta146@gmail.com>
2021-07-23 20:10:20 +0530

    added jest and test cases

---
+++ functions.test.js
@@ -0,0 +1,34 @@
+const functions =  require('./functions');
+
+test('Adds 2+2 to equal 4',()=>{
+  expect(functions.add(2,2)).toBe(4);
+})
+
+
+test('Adds 2+2 to equal 4', ()=>{
+  expect(functions.add(2, 2)).not.toBe(5);
+})
+
+test('Should be null', ()=>{
+  expect(functions.isNull()).toBeNull();
+})
+
+test('Should be null', ()=>{
+  expect(functions.isNull()).not.toBeUndefined();
+})
+
+test('Should be falsy', ()=>{
+  expect(functions.checkValue(null)).toBeFalsy();
+})
+
+test('Should be falsy', ()=>{
+  expect(functions.checkValue(0)).toBeFalsy();
+})
+
+test('Should be falsy', ()=>{
+  expect(functions.checkValue(undefined)).toBeFalsy();
+})
+
+test('Should be truthy', ()=>{
+  expect(functions.checkValue(1)).toBeTruthy();
+})
